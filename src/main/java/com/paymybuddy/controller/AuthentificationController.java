186b70310c1a9838d556d797502631b0bc987886
Fethi KHARRAT <fethi.kharrat@gmail.com>
2025-04-22 17:47:55 +0200

    affichage Bouton Pay My Buddy dans la page HTML d'inscription

--- src/main/java/com/paymybuddy/controller/AuthentificationController.java
+++ src/main/java/com/paymybuddy/controller/AuthentificationController.java
@@ -3,17 +3,27 @@ package com.paymybuddy.controller;
 import com.paymybuddy.dto.UserRegistrationDTO;
 import com.paymybuddy.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthentificationController {
 
     private final UserService userService;
 
+    // Affiche le formulaire d'inscription
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new UserRegistrationDTO());
+        return "register";
+    }
+
     @PostMapping("/register")
     public String register(@RequestBody UserRegistrationDTO dto) {
+
         return userService.register(dto);
     }
 }
